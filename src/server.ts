a8dfc0c0cee6c6176b9036771c83921dcb3dc08a
Th0u305 <ahnaf4411@gmail.com>
2025-07-09 09:30:08 +0600

    server init

---
+++ src/server.ts
@@ -0,0 +1,69 @@
+/* eslint-disable no-console */
+import { Server } from "http";
+import mongoose from "mongoose";
+import { envVars } from "./config/env";
+import app from "./app";
+
+let server: Server;
+
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(envVars.DB_URL)        
+        server = app.listen(envVars.PORT, () => {
+          console.log(`App is listing on port ${envVars.PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
+
+process.on("unhandledRejection" ,(error)=>{
+    console.log("uncaughtException detected ...Server shutting down..", error);
+    if (server) {
+        server.close(()=>{
+            process.exit(1)
+        })
+    } 
+    process.exit(1)
+})
+
+
+process.on("uncaughtException" ,(error)=>{
+    console.log("uncaughtException detected ...Server shutting down..", error);
+    if (server) {
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+
+process.on("SIGTERM" ,(error)=>{
+    console.log("SIGTERM signal received ...Server shutting down..", error);
+    if (server) {
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+process.on("SIGINT" ,(error)=>{
+    console.log("SIGINT signal received ...Server shutting down..", error);
+    if (server) {
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+// unhandled rejection error
+// Promise.reject(new Error ("I forgot to catch this error"))
+
+// uncaught exception error 
+// throw new Error("I forgot to catch this error")
